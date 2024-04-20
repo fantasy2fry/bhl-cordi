3e2196c19654ada76356613fffabc4d754719aaf
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2024-04-20 18:25:09 +0200

    translate parameter change

--- base/classes/translator.py
+++ base/classes/translator.py
@@ -5,8 +5,8 @@ class Translator:
     def __init__(self, lang):
         self.lang = lang
 
-    def translate(self, text_list, lang):
-        t = TranslateLibTranslator(to_lang=lang)
+    def translate(self, text_list):
+        t = TranslateLibTranslator(to_lang=self.lang)
         translated_text = []
         for i in range(len(text_list)):
             translated_text.append(t.translate(text_list[i]))
