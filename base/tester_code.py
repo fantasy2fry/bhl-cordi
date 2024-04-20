f1ee73b150f37091797b7db9fa5b3d43b9611d2f
Igor <igorrudolf2003@gmail.com>
2024-04-20 18:56:00 +0200

    added analyse oop, problem with tab structure

--- base/tester_code.py
+++ base/tester_code.py
@@ -1,7 +1,7 @@
 class User:
     def __init__(self, name, age):
         self.name = name  # powinno być self._name
-        self.age = age    # powinno być self._age
+        self.age = age  # powinno być self._age
 
     def display_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
@@ -9,6 +9,7 @@ class User:
     def birthday(self):
         self.age += 1
         print(f"Happy birthday {self.name}, now you are {self.age} years old!")
+
 
 # Użycie klasy
 user = User("John Doe", 30)
