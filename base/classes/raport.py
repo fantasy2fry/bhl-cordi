4452fdc88a269871cabcc2b97129f83f15bd5372
SealyPrograming <lafar@DRAK01.(none)>
2024-04-21 10:54:56 +0200

    updated 3 raport class

--- base/classes/raport.py
+++ base/classes/raport.py
@@ -2,11 +2,17 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from translator import Translator
 from operator import itemgetter
+from reportlab.lib.colors import HexColor
 import re
 
 
 class Raport:
-    def create_pdf_from_text(self, tab_topic, tab_description, det_description, det_detailed_descriptor, filename, lang):
+    def create_pdf_from_text(self, tab_topic, tab_description,
+                             det_description, det_detailed_descriptor,
+                             csrf_description, csrf_detailed_description,
+                             insecure_deserialization_description, insecure_deserialization_detailed_description,
+                             xss_detector_description, xss_detector_detailed_description, tab_solutions,
+                             filename, lang):
         # t = Translator(lang)
         # tab_description = t.translate_with_exclusions(tab_description, self.extract_words_from_description(" ".join(tab_description)))
         # print(tab_description)
@@ -15,23 +21,39 @@ class Raport:
         for topic, desc in zip(tab_topic, tab_description):
             if topic not in tab_description_dict:
                 tab_description_dict[topic] = []
-            tab_description_dict[topic].append(desc)
+            tab_description_dict[topic].append("• " + desc)
+        tab_solutions = ["• " + tab_solutions[0], "• " + tab_solutions[1]]
+        pos = list(tab_description_dict.keys()).index('Static variable issue')
+        items = list(tab_description_dict.items())
+        items.insert(pos, ('Solutions', tab_solutions))
+        tab_description_dict = dict(items)
         for topic, desc in zip(det_description, list(map(itemgetter(0), det_detailed_descriptor))):
             if topic not in tab_description_dict:
                 tab_description_dict[topic] = []
             tab_description_dict[topic].append(desc)
+        for topic, desc in zip(csrf_description, list(map(itemgetter(0), csrf_detailed_description))):
+            if topic not in tab_description_dict:
+                tab_description_dict[topic] = []
+            tab_description_dict[topic].append(desc)
+        for topic, desc in zip(insecure_deserialization_description, list(map(itemgetter(0), insecure_deserialization_detailed_description))):
+            if topic not in tab_description_dict:
+                tab_description_dict[topic] = []
+            tab_description_dict[topic].append(desc)
+        # for topic, desc in zip(xss_detector_description, list(map(itemgetter(0), xss_detector_detailed_description))):
+        #     if topic not in tab_description_dict:
+        #         tab_description_dict[topic] = []
+        #     tab_description_dict[topic].append(desc)
 
         sections = list(tab_description_dict.items())  # Convert dict items to list of tuples
-        print(tab_description_dict)
-        self._generate_pdf(sections, filename)
+        self._generate_pdf(sections, filename, tab_solutions)
 
     def extract_words_from_description(self, tab_description):
         words = re.findall(r"'(\w+)'", tab_description)
         return words
 
-    def _generate_pdf(self, sections, filename):
+    def _generate_pdf(self, sections, filename, tab_solutions):
         c = canvas.Canvas(filename, pagesize=letter)
-        c.setFont("Helvetica", 12)
+        c.setFont("Helvetica", 8)
 
         text_height = 750
         y_position = text_height
@@ -42,41 +64,57 @@ class Raport:
         c.drawCentredString(letter[0] / 2, y_position, title)
         y_position -= 60
 
-        c.setFont("Helvetica-Bold", 16)  # Larger font size for the title
-        title = "OOP oriented analysis"
-        c.drawCentredString(letter[0] / 2, y_position, title)
-        y_position -= 40
-
         for topic, descriptions in sections:
             # Check if there's enough space for the section title
             if y_position < 50:
                 c.showPage()  # Start a new page
-                c.setFont("Helvetica", 12)  # Reset font
+                c.setFillColor(HexColor(0x000000))
+                c.setFont("Helvetica", 8)  # Reset font
                 y_position = text_height  # Reset y_position
 
-            c.setFont("Helvetica-Bold", 14)
+            c.setFont("Helvetica-Bold", 10)
+            c.setFillColor(HexColor(0x000000))
             c.drawString(50, y_position, topic)
             y_position -= 20
 
-            c.setFont("Helvetica", 12)
-            for desc in descriptions:
-                lines = self.wrap_text(desc, max_width=500)  # Adjust max_width as needed
-                for line in lines:
-                    # Check if there's enough space for the current line
-                    if y_position < 50:
-                        c.showPage()  # Start a new page
-                        c.setFont("Helvetica", 12)  # Reset font
-                        y_position = text_height  # Reset y_position
+            if topic == "Solutions":
+                c.setFillColor(HexColor(0x0096C7))
+                c.setFont("Helvetica", 8)
+                for desc in descriptions:
+                    lines = self.wrap_text(desc, max_width=500)  # Adjust max_width as needed
+                    for line in lines:
+                        # Check if there's enough space for the current line
+                        if y_position < 50:
+                            c.showPage()  # Start a new page
+                            c.setFillColor(HexColor(0x0096C7))
+                            c.setFont("Helvetica", 8)  # Reset font
+                            y_position = text_height  # Reset y_position
 
-                    self.draw_text_with_bold(line, c, y_position)
-                    y_position -= 15
+                        self.draw_text_with_bold(line, c, y_position)
+                        y_position -= 20
+            else:
+                c.setFillColor(HexColor(0x000000))
+                c.setFont("Helvetica", 8)
+                for desc in descriptions:
+                    lines = self.wrap_text(desc, max_width=500)  # Adjust max_width as needed
+                    for line in lines:
+                        # Check if there's enough space for the current line
+                        if y_position < 50:
+                            c.showPage()  # Start a new page
+                            c.setFillColor(HexColor(0x000000))
+                            c.setFont("Helvetica", 8)  # Reset font
+                            y_position = text_height  # Reset y_position
+
+                        self.draw_text_with_bold(line, c, y_position)
+                        y_position -= 20
 
             y_position -= 20  # Additional space after each section
 
-        c.setFont("Helvetica-Bold", 16)  # Larger font size for the title
-        title = "Vulnerabilities oriented analysis"
-        c.drawCentredString(letter[0] / 2, y_position, title)
-        y_position -= 40
+        # t = c.beginText()
+        # t.setFont('Helvetica-Bold', 6)
+        # t.setCharSpace(3)
+        # t.textLine(tab_solutions[0])
+        # c.drawText(t)
 
         c.save()
         print("PDF created successfully!")
@@ -102,8 +140,8 @@ class Raport:
 
     def draw_text_with_bold(self, text, canvas_obj, y_position):
         font = "Helvetica"
-        font_size = 12
-        bold_font_size = 12
+        font_size = 8
+        bold_font_size = 8
         x_position = 50  # Starting x-coordinate for drawing text
 
         in_quotes = False
@@ -140,18 +178,35 @@ class Raport:
 
     def get_text_width(self, text):
         c = canvas.Canvas("tmp.pdf")
-        return c.stringWidth(text, "Helvetica", 12)
+        return c.stringWidth(text, "Helvetica", 10)
 
 
 if __name__ == "__main__":
     from analyze_oop import *
-    from analyze_vuln import SQLInjectionDetector
+    from analyze_vuln import *
     basic_object = BasicAnalyserOOP('../../additional', 1)
+
+
     detector = SQLInjectionDetector()
     detector.analyze_directory('../../additional')
+    csrf = CSRFDetector()
+    csrf.analyze_directory('../../additional')
+    insecure_deserialization = InsecureDeserializationDetector()
+    insecure_deserialization.analyze_directory('../../additional')
+    xss_detector = XSSDetector()
+    xss_detector.analyze_directory('../../additional')
+
+    # print("\n")
+    # print("\n")
+    # print(len(basic_object.tab_solution))
+    # print("\n")
+    #
+    # print(basic_object.tab_solution)
+
 
     raport = Raport()
-
-    print(basic_object.tab_description)
-
-    raport.create_pdf_from_text(basic_object.tab_topic, basic_object.tab_description, detector.description, detector.detailed_description, "raport.pdf", "ru")
+    raport.create_pdf_from_text(basic_object.tab_topic, basic_object.tab_description,
+                                detector.description, detector.detailed_description,
+                                csrf.description, csrf.detailed_description,
+                                insecure_deserialization.description, insecure_deserialization.detailed_description,
+                                xss_detector.description, xss_detector.detailed_description, basic_object.tab_solution, "raport.pdf", "en")
